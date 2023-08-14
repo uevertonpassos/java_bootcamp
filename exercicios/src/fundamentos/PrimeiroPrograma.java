99646f98c7dd556477f32f5ef083d15c4d6e9480
Ueverton Passos <uevertonpassos96@gmail.com>
2023-08-14 11:46:50 -0300

    hahahahah

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		//testing comentarios
+		System.out.println("primeiro programa!");
+		/*
+		 * pode ser assim
+		 * também, para comentar
+		 * textos
+		 */
+	}
+
+}
