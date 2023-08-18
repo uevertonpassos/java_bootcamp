5721e96a5eb0b0e734da1932ef7855d27247f1b7
Ueverton Passos <uevertonpassos96@gmail.com>
2023-08-18 02:01:10 -0300

    Brincando com métodos de uma classe JAVA

--- java-anatoma-classes/AnatomiaClasses/src/MinhaClasse.java
+++ java-anatoma-classes/AnatomiaClasses/src/MinhaClasse.java
@@ -1,5 +1,20 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Começando o bootcamp!");
+
+        // criando variáveis e chamando dentro do método criado abaixo
+
+        String PrimeiroNome = "Ueverton";
+        String SegundoNome = "Passos";
+
+        String NomeCompleto = NomeCompleto(PrimeiroNome, SegundoNome);
+
+        System.out.println(NomeCompleto);
+    }
+
+    // Aprendendo a usar métodos em Java
+    public static String NomeCompleto(String PrimeiroNome, String SegundoNome){
+
+        return "O nome completo é: " + PrimeiroNome + " " + SegundoNome;
+
     }
 }
