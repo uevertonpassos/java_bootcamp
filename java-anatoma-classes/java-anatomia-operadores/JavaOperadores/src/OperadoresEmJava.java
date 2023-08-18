cea20befe82e3d246f7009861821a82d5daff3c3
Ueverton Passos <uevertonpassos96@gmail.com>
2023-08-18 16:10:45 -0300

    soma entre valores

--- java-anatoma-classes/java-anatomia-operadores/JavaOperadores/src/OperadoresEmJava.java
+++ java-anatoma-classes/java-anatomia-operadores/JavaOperadores/src/OperadoresEmJava.java
@@ -1,5 +1,15 @@
 public class OperadoresEmJava {
 
+   public static void main(String[] args) {
+    int NumeroUm = 12;
+    int NumeroDois = 13;
+
+    // transformando os valores em decimais
+    double soma =NumeroUm + NumeroDois;
+
+    System.out.println("A soma entre os valores é: " + soma);
+
     
+   }
     
 }
