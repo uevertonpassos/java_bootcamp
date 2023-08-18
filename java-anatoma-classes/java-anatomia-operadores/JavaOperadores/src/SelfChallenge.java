b40505c75e951bc8572323285e2ecbd6aed42ed0
Ueverton Passos <uevertonpassos96@gmail.com>
2023-08-18 16:39:58 -0300

    finalizando o dia?

---
+++ java-anatoma-classes/java-anatomia-operadores/JavaOperadores/src/SelfChallenge.java
@@ -0,0 +1,25 @@
+public class SelfChallenge {
+
+    public static void main(String[] args) {
+        double NumeroUm = 10;
+        double NumeroDois = 8;
+        double NumeroTres = 6;
+        double NumeroQuatro = 7;
+        double Media = Media(NumeroUm, NumeroDois, NumeroTres, NumeroQuatro);
+
+        System.out.println("A média dos valores equivale a: " + Media);
+
+
+        
+    }
+
+
+    public static double Media(double NumeroUm, double NumeroDois, double NumeroTres, double NumeroQuatro){
+
+
+        // não posso retornar um texto junto com uma equação desse tipo
+        return  (NumeroUm + NumeroDois+NumeroTres+NumeroQuatro)/4;
+
+    }
+    
+}
