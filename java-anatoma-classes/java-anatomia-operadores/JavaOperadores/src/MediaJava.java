ca2d7590fd069c1c2921c70566d72c6e77300dbf
Ueverton Passos <uevertonpassos96@gmail.com>
2023-08-18 16:18:31 -0300

    javeirinho ele

---
+++ java-anatoma-classes/java-anatomia-operadores/JavaOperadores/src/MediaJava.java
@@ -0,0 +1,14 @@
+public class MediaJava {
+
+    public static void main(String[] args) {
+        double NotaUm = 8.0;
+        double NotaDois = 6.0;
+        double NotaTres = 7.0;
+        double NotaQuatro = 9.0;
+
+        double Media = (NotaUm + NotaDois + NotaTres + NotaQuatro) / 4;
+
+        System.out.println("A média final do aluno será: " + Media);
+    }
+    
+}
