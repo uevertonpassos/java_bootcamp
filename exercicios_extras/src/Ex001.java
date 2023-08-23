13d029d52be1657ae64b3af52da5251d6123dc4b
Ueverton <uevertonpassos96@gmail.com>
2023-08-23 12:12:23 -0300

    alguns exercicios java

---
+++ exercicios_extras/src/Ex001.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex001 {
+
+    //Escreva um programa em Java que recebe a idade de uma pessoa e verifica se ela é maior de idade (idade maior ou igual a 18) ou menor de idade.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insert ur age: ");
+        int idade = scanner.nextInt();
+
+        if (idade >= 18)
+            System.out.println("the user is at the legal age");
+        else
+            System.out.println("the user is not at the legal age");
+
+    }
+
+
+
+    
+}
---
+++ exercicios_extras/src/Ex002.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ex002 {
+
+   
+//Crie um programa que determine se um número inteiro inserido pelo usuário é par ou ímpar usando estruturas de controle if/else.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Insert a number: ");
+        int valor = scanner.nextInt();
+
+        if (valor % 2 == 0)
+            System.out.println("thi number is even!");
+        else
+            System.out.println("this number is odd");
+        
+    }
+}
