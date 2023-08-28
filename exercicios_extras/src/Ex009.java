b8fc03138c8bfa34d150a468e3bf701f721b24ac
Ueverton <uevertonpassos96@gmail.com>
2023-08-28 10:38:46 -0300

    número mágico em Java

---
+++ exercicios_extras/src/Ex009.java
@@ -0,0 +1,33 @@
+//Crie um programa que peça ao usuário para adivinhar um número secreto entre 1 e 100. Use um loop while para dar dicas se o número fornecido é maior ou menor que o número secreto, até que o usuário acerte.
+import java.util.Scanner;
+public class Ex009 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int MagicNumber = 14;
+
+       while(true){
+        System.out.print("Digite um numero entre 1 e 100 e tente acertar o número mágico: ");
+        int usuario = scanner.nextInt();
+        if(usuario < MagicNumber){
+            System.out.println("Você chutou um número menor que o número mágico");
+            
+        }
+        else if(usuario > MagicNumber){
+            System.out.println("Você chutou um número maior que o número mágico");
+
+        }
+        else{
+            System.out.println("Você acertou!");
+            break;
+            
+        }
+       }
+
+
+
+
+    }
+    
+}
